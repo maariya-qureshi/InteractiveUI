ef3d56f601489116f9716b7780edeb3e3e0d266a
Maariya Qureshi <maariyaqureshi1@gmail.com>
2025-07-09 12:34:33 -0400

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Maariya Qureshi on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
