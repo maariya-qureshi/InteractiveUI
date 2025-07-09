f6f9c9b0d863365ba9562fe1b2b7270b3c9fb6ba
Maariya Qureshi <maariyaqureshi1@gmail.com>
2025-07-09 13:38:53 -0400

    added in private vars, and an action in the button to display Welcome (name)

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //storing variable for the input in the button
+    //property wrappers: gives var additional behavior, ex: @State -> makes swift update it once it is changed
+    //private prevents it from being accessed outside
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
     var body: some View {
         VStack {
-            Text("What is your name?")
-                .font(.title)
+            Text(textTitle) //var above
+                .font(.title) //this is a modifier
             
-            TextField("Type name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type name here...", text: $name)
+            //$name: stores values inside the name property (var)
                 .multilineTextAlignment(.center)
                 .font(.title)
-                .border(Color.gray, width: 1)
+                .border(Color.pink, width: 3)
             
             //Button: string that makes up the text, and inside: an action
+            
             Button("Submit Name") {
-                
+                textTitle = "Welcome \(name)!"
+                //this references back that variable and changes it to read welcome _ name
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
